8c2f9c4d26e815015da910f2658f1bf873095258
kartikk-k <hellokartikk@gmail.com>
2026-04-08 09:01:13 +0530

    Initial Commit

---
+++ Mac native speech to text/Mac_native_speech_to_textApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_native_speech_to_textApp.swift
+//  Mac native speech to text
+//
+//  Created by Kartik Khorwal on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Mac_native_speech_to_textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
